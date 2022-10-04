f053a781b2dc6db6c6e9547a90256f1008eea79e
Duakhan13 <duakhan2652@gmail.com>
2022-10-04 13:55:18 +0500

    if statement lecture completed

---
+++ IfStatementsAndOperators/IntroToIfStatements.js
@@ -0,0 +1,12 @@
+/** @format */
+
+//introduction to if else statements
+//tells the code if this thing is true, run more code
+//if (expression) {
+//statement(s) to be executed if expression
+// evaluates to truthy or falsey}
+
+const shouldSayHi = true;
+if (shouldSayHi) {
+  console.log("Hi there!");
+}
